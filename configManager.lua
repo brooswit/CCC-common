6b6809a42b9922ef01b765025ff3917987de306b
John Winstead <brooswit@gmail.com>
2021-07-13 11:53:00 -0700

    Update configManager.lua

--- configManager.lua
+++ configManager.lua
@@ -1,5 +1,5 @@
-os.loadAPI('fileManager')
-os.loadAPI('json')
+os.loadAPI('fileManager.lua')
+os.loadAPI('json.lua')
 
 function load(configName)
     json.decode(fileManager.load(configName .. '.json'))
