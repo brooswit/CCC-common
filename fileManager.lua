0fd9019073ac343d00398236a7eeec55cdd73668
John Winstead <brooswit@gmail.com>
2021-07-13 20:08:44 -0700

    Update fileManager.lua

--- fileManager.lua
+++ fileManager.lua
@@ -1,11 +1,17 @@
 function save(fileName, payload)
   local file = fs.open(fileName, "w")
+  if file == nil then
+    return
+  end
   file.write(payload)
   file.close()
 end
 
 function load(fileName)
   local file = fs.open(fileName, "r")
+  if file == nil then
+    return nil
+  end
   local payload = file.readAll()
   file.close()
   return payload
