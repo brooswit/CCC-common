c3f220bbcd78deefb6fae8c28e66a41f79bc70ee
John Winstead <brooswit@gmail.com>
2021-07-19 15:34:49 -0700

    Add logging

--- configManager.lua
+++ configManager.lua
@@ -1,10 +1,15 @@
 os.loadAPI('fileManager.lua')
 os.loadAPI('json.lua')
 
+local function localPrint(msg)
+    print ("[ConfigManager] " .. msg)
+end
 function load(configName)
+    localPrint ("Loading  " .. configName)
     json.decode(fileManager.load(configName .. '.json'))
 end
 
 function save(configName, payload)
+    localPrint ("Saving  " .. configName)
     fileManager.save(configName .. '.json', json.decode(payload))
 end
--- fileManager.lua
+++ fileManager.lua
@@ -1,18 +1,26 @@
-function save(fileName, payload)
-  local file = fs.open(fileName, "w")
-  if file == nil then
-    return
-  end
-  file.write(payload)
-  file.close()
+local function localPrint(msg)
+  print ("[FileManager] " .. msg)
 end
 
 function load(fileName)
+  localPrint ("Loading  " .. fileName)
   local file = fs.open(fileName, "r")
   if file == nil then
+    localPrint ("Failed to load. file is nil.")
     return nil
   end
   local payload = file.readAll()
   file.close()
   return payload
+end
+
+function save(fileName, payload)
+  localPrint ("Saving  " .. fileName)
+  local file = fs.open(fileName, "w")
+  if file == nil then
+    localPrint ("Failed to save. file is nil.")
+    return
+  end
+  file.write(payload)
+  file.close()
 end
